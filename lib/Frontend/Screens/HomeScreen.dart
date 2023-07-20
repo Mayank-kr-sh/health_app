fe9fc10d1f38de1c95204e665de2f5e1a9660cdf
Mayank Kumar Shah <mayank.k.iiit@gmail.com>
2023-07-20 18:04:08 +0530

    Changes UI

--- lib/Frontend/Screens/HomeScreen.dart
+++ lib/Frontend/Screens/HomeScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
 import '../Widgets/video_popup.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -10,17 +12,28 @@ class HomeScreen extends StatelessWidget {
     final screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
+      appBar: AppBar(
+        title: const Text(
+          'Home',
+          style: TextStyle(color: Colors.black),
+        ),
+        elevation: 2,
+        centerTitle: true,
+        backgroundColor: const Color(0xff5cb85c),
+      ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image.asset(
-              'assets/images/logo.png',
-              width: screenWidth * 0.2,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SvgPicture.asset(
+                'assets/images/logo2.svg',
+                width: screenWidth * 0.4,
+              ),
             ),
             Container(
-              height: screenHeight * 0.43,
+              height: screenHeight * 0.48,
               decoration: const BoxDecoration(
                 image: DecorationImage(
                   image: AssetImage(
@@ -29,80 +42,104 @@ class HomeScreen extends StatelessWidget {
                   fit: BoxFit.cover,
                 ),
               ),
-              child: Container(
-                color: Colors.lightGreen.withOpacity(0.7),
-                padding: const EdgeInsets.all(8),
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.all(12),
-                        child: Text(
-                          'A paragraph is a series of sentences that are organized and coherent, and are all related to a single topic. Almost every piece of writing you do that is longer than a few sentences should be organized into paragraphs. This is because paragraphs show a reader where the subdivisions of an essay begin and end, and thus help the reader see the organization of the essay and grasp its main points.',
-                          textAlign: TextAlign.justify,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 10),
-                        child: ElevatedButton(
-                          onPressed: () {
-                            // Handle button click here
-                          },
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.green,
-                            padding: const EdgeInsets.symmetric(vertical: 16),
-                            minimumSize: Size(screenWidth * 0.8, 0),
-                          ),
-                          child: const Text(
-                            'Get Started for Free',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
+              child: OrientationBuilder(
+                builder: (context, orientation) {
+                  return Container(
+                    color: const Color(0xff188ba0).withOpacity(0.8),
+                    padding: const EdgeInsets.all(8),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.all(12),
+                            child: Text(
+                              "Halemind is an ingenious, leading edge and connected EMR and HMS that empowers independent practices and hospitals. Halemind's secure, advanced and speciality driven EHR that enables you to run your practice in a simpler and efficient way, so you can focus on what matters the most - Your Patients.",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                              textAlign: TextAlign.justify,
                             ),
                           ),
-                        ),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          showDialog(
-                            context: context,
-                            barrierDismissible: true,
-                            builder: (BuildContext context) {
-                              return VideoPlayerDialog(
-                                videoUrl:
-                                    'https://www.youtube.com/watch?v=tnxGPvZcOWk',
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 10),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                // Handle button click here
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color(0xff52c1ac),
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 16),
+                                minimumSize: Size(
+                                  orientation == Orientation.portrait
+                                      ? screenWidth * 0.6
+                                      : screenHeight * 0.8,
+                                  0,
+                                ),
+                              ),
+                              child: const Text(
+                                'Get Started for Free',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              showDialog(
+                                context: context,
+                                barrierDismissible: true,
+                                builder: (BuildContext context) {
+                                  return VideoPlayerDialog(
+                                    videoUrl:
+                                        'https://www.youtube.com/watch?v=tnxGPvZcOWk',
+                                  );
+                                },
                               );
                             },
-                          );
-                        },
-                        child: Container(
-                          height: screenHeight * 0.18,
-                          width: screenWidth * 0.7,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.white, width: 2),
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(3.0),
                             child: Container(
+                              height: orientation == Orientation.portrait
+                                  ? screenHeight * 0.25
+                                  : screenHeight * 0.6,
+                              width: orientation == Orientation.portrait
+                                  ? screenWidth * 0.9
+                                  : screenWidth * 0.48,
                               decoration: BoxDecoration(
                                 border:
                                     Border.all(color: Colors.white, width: 2),
-                                borderRadius: BorderRadius.circular(6),
+                                borderRadius: BorderRadius.circular(8),
                               ),
-                              padding: const EdgeInsets.all(7),
-                              child: Image.asset(
-                                'assets/images/poster2.png',
-                                width: screenWidth * 0.8 - 10,
-                                height: screenHeight * 0.18 - 10,
+                              child: Padding(
+                                padding: const EdgeInsets.all(3.0),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color: Colors.white, width: 2),
+                                    borderRadius: BorderRadius.circular(6),
+                                  ),
+                                  padding: const EdgeInsets.all(7),
+                                  child: Image.asset(
+                                    'assets/images/thumbnail.png',
+                                    width: orientation == Orientation.portrait
+                                        ? screenWidth * 0.7 - 10
+                                        : screenWidth * 0.6 - 10,
+                                    height: orientation == Orientation.portrait
+                                        ? screenHeight * 0.3 - 10
+                                        : screenHeight * 0.4 - 10,
+                                  ),
+                                ),
                               ),
                             ),
                           ),
-                        ),
+                        ],
                       ),
-                    ],
-                  ),
-                ),
+                    ),
+                  );
+                },
               ),
             ),
           ],
